e1ab369319307db062d40742007cff83863831bb
Jakub Zalas <jakub@zalas.pl>
2015-08-25 18:47:49 +0100

    [3.3] Move the UserRepository to the infrastrcuture namespace

--- src/Infrastructure/Doctrine/Repository/UserRepository.php
+++ src/Infrastructure/Doctrine/Repository/UserRepository.php
@@ -9,7 +9,7 @@
  * file that was distributed with this source code.
  */
 
-namespace AppBundle\Repository;
+namespace Infrastructure\Doctrine\Repository;
 
 use AppBundle\Entity\User;
 use AppBundle\Entity\UserRepository as AppUserRepository;
