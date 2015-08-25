12e84e86452389eec5f598843d8e8091f711e05f
Jakub Zalas <jakub@zalas.pl>
2015-08-25 10:32:45 +0100

    [1.5] Create a custom user provider based on the user repository

---
+++ src/AppBundle/Security/User/UserProvider.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace AppBundle\Security\User;
+
+use AppBundle\Entity\User;
+use AppBundle\Repository\UserRepository;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+
+class UserProvider implements UserProviderInterface
+{
+    /**
+     * @var UserRepository
+     */
+    private $userRepository;
+
+    /**
+     * @param UserRepository $userRepository
+     */
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    /**
+     * @param string $username
+     *
+     * @return UserInterface
+     *
+     * @throws UsernameNotFoundException if the user is not found
+     */
+    public function loadUserByUsername($username)
+    {
+        $user = $this->userRepository->findOneByUsername($username);
+
+        if (null === $user) {
+            throw new UsernameNotFoundException(sprintf('User "%s" not found.', $username));
+        }
+
+        return $user;
+    }
+
+    /**
+     * @param UserInterface $user
+     *
+     * @return UserInterface
+     *
+     * @throws UnsupportedUserException if the account is not supported
+     */
+    public function refreshUser(UserInterface $user)
+    {
+        if (!$user instanceof User) {
+            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', get_class($user)));
+        }
+
+        return $this->loadUserByUsername($user->getUsername());
+    }
+
+    /**
+     * @param string $class
+     *
+     * @return bool
+     */
+    public function supportsClass($class)
+    {
+        return $class === User::class;
+    }
+}
---
+++ src/AppBundle/Tests/Security/User/UserProviderTest.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace AppBundle\Tests\Security\User;
+
+use AppBundle\Entity\User;
+use AppBundle\Repository\UserRepository;
+use AppBundle\Security\User\UserProvider;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+
+class UserProviderTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var UserProviderInterface
+     */
+    private $userProvider;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $userRepository;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject
+     */
+    private $user;
+
+    protected function setUp()
+    {
+        $this->userRepository = $this->getMockBuilder(UserRepository::class)
+            ->disableOriginalConstructor()
+            ->setMethods(['findOneByUsername'])
+            ->getMock();
+        $this->userProvider = new UserProvider($this->userRepository);
+
+        $this->user = $this->getMock(User::class);
+        $this->user->expects($this->any())
+            ->method('getUsername')
+            ->willReturn('kuba');
+    }
+
+    public function testItIsAUserProvider()
+    {
+        $this->assertInstanceOf(UserProviderInterface::class, $this->userProvider);
+    }
+
+    public function testItLoadsUserByUsername()
+    {
+        $this->userRepository->expects($this->any())
+            ->method('findOneByUsername')
+            ->with('kuba')
+            ->willReturn($this->user);
+
+        $this->assertSame($this->user, $this->userProvider->loadUserByUsername('kuba'));
+    }
+
+    /**
+     * @expectedException \Symfony\Component\Security\Core\Exception\UsernameNotFoundException
+     */
+    public function testItThrowsAUsernameNotFoundExceptionIfUserCannotBeLoaded()
+    {
+        $this->userRepository->expects($this->any())
+            ->method('findOneByUsername')
+            ->with('kuba')
+            ->willReturn(null);
+
+        $this->userProvider->loadUserByUsername('kuba');
+    }
+
+    public function testItRefreshesTheUser()
+    {
+        $reloadedUser = $this->getMock(User::class);
+        $this->userRepository->expects($this->any())
+            ->method('findOneByUsername')
+            ->with('kuba')
+            ->willReturn($reloadedUser);
+
+        $this->assertSame($reloadedUser, $this->userProvider->refreshUser($this->user));
+    }
+
+    /**
+     * @expectedException \Symfony\Component\Security\Core\Exception\UnsupportedUserException
+     */
+    public function testItThrowsAnUnsupportedUserExceptionForNonAppBundleUser()
+    {
+        $this->userRepository->expects($this->any())
+            ->method('findOneByUsername')
+            ->willReturn($this->getMock(User::class));
+
+        $this->userProvider->refreshUser($this->getMock(UserInterface::class));
+    }
+
+    public function testItSupportsTheAppBundleUser()
+    {
+        $this->assertTrue($this->userProvider->supportsClass(User::class));
+    }
+
+    public function testItDoesNotSupportOtherClasses()
+    {
+        $this->assertFalse($this->userProvider->supportsClass(UserInterface::class));
+    }
+}
