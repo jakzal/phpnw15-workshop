161c112b534ce067dc7058508c3e55ff48898062
Jakub Zalas <jakub@zalas.pl>
2015-10-01 20:36:09 +0100

    Add a test for the ArticleListCommand class

---
+++ tests/CrawlerBundle/Command/ArticleListCommandTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace CrawlerBundle\Command;
+
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+
+/**
+ * @group integration
+ */
+class ArticleListCommandTest extends \PHPUnit_Framework_TestCase
+{
+    const HOST = 'http://127.0.0.1:8000/';
+
+    protected function setUp()
+    {
+        $headers = @get_headers(self::HOST);
+
+        if (!$headers) {
+            $this->markTestSkipped('The PHP server is not running on ', self::HOST);
+        }
+    }
+
+    public function testExecuteReturnsZeroStatusCodeIfSuccessful()
+    {
+        $this->markTestSkipped('This test is waiting to be implemented.');
+
+        $commandTester = $this->executeCommand();
+
+        $this->assertSame(0, $commandTester->getStatusCode());
+    }
+
+    public function testItListsFoundArticles()
+    {
+        $this->markTestSkipped('This test is waiting to be implemented.');
+
+        $commandTester = $this->executeCommand();
+
+        $this->assertRegExp('/Morbi tempus commodo mattis/smi', $commandTester->getDisplay());
+    }
+
+    /**
+     * @return CommandTester
+     */
+    public function executeCommand()
+    {
+        $application = new Application();
+        $application->add(new ArticleListCommand());
+
+        $command = $application->find('article:list');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(['command' => $command->getName()], ['env' => 'test']);
+
+        return $commandTester;
+    }
+}
