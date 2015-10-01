1805c8d0f7130441bc8e07840aefd51a4fe83970
Jakub Zalas <jakub@zalas.pl>
2015-10-01 19:51:52 +0100

    [Crawler][1.2] Make the first test pass

--- src/Crawler/ArticleListCrawler.php
+++ src/Crawler/ArticleListCrawler.php
@@ -5,11 +5,34 @@ namespace Crawler;
 class ArticleListCrawler
 {
     /**
+     * @var ContentProvider
+     */
+    private $contentProvider;
+
+    /**
+     * @var ArticleExtractor
+     */
+    private $articleExtractor;
+
+    /**
+     * @param ContentProvider $contentProvider
+     * @param ArticleExtractor $articleExtractor
+     */
+    public function __construct(ContentProvider $contentProvider, ArticleExtractor $articleExtractor)
+    {
+        $this->contentProvider = $contentProvider;
+        $this->articleExtractor = $articleExtractor;
+    }
+
+    /**
      * @param string $resource
      *
      * @return Article[]
      */
     public function findArticles($resource)
     {
+        $content = $this->contentProvider->fetch($resource);
+
+        return $this->articleExtractor->extractArticles($content);
     }
 }
