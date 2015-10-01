67db62aff0d27d18bbabfca62a6998e13e56ddbd
Jakub Zalas <jakub@zalas.pl>
2015-10-01 19:58:04 +0100

    Add a test for the ArticleExtractor class

---
+++ tests/Crawler/ArticleExtractorTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Crawler;
+
+class ArticleExtractorTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @expectedException \LogicException
+     * @expectedExceptionMessage Could not find articles in the content:
+     */
+    public function testItThrowsAnExceptionIfNoArticleWasFound()
+    {
+        $this->markTestSkipped('This test is waiting to be implemented.');
+
+        $content = '<article><h2><a href="/en/blog/posts/foo">Foo</a></h2></article>';
+
+        $articleExtractor = new ArticleExtractor();
+        $articleExtractor->extractArticles($content);
+    }
+
+    public function testItReturnsListOfArticles()
+    {
+        $this->markTestSkipped('This test is waiting to be implemented.');
+
+        $content =
+            '<article class="post"><h2><a href="/en/blog/posts/foo">Foo</a></h2></article>
+             <article class="post"><h2><a href="/en/blog/posts/bar">Bar</a></h2></article>';
+
+        $articleExtractor = new ArticleExtractor();
+        $articles = $articleExtractor->extractArticles($content);
+
+        $this->assertInternalType('array', $articles);
+        $this->assertEquals([new Article('Foo', '/en/blog/posts/foo'), new Article('Bar', '/en/blog/posts/bar')], $articles);
+    }
+
+    public function testItTrimsTheTitles()
+    {
+        $this->markTestSkipped('This test is waiting to be implemented.');
+
+        $content =
+            '<article class="post"><h2><a href="/en/blog/posts/foo">
+                Foo
+            </a></h2></article>';
+
+        $articleExtractor = new ArticleExtractor();
+        $articles = $articleExtractor->extractArticles($content);
+
+        $this->assertInternalType('array', $articles);
+        $this->assertEquals([new Article('Foo', '/en/blog/posts/foo')], $articles);
+    }
+}
