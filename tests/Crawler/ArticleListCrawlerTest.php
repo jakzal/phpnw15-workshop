32c9df6fa50c4249b6bb2a0dc28132902fd43efc
Jakub Zalas <jakub@zalas.pl>
2015-10-01 19:41:49 +0100

    Add a test for the ArticleListCrawler class

---
+++ tests/Crawler/ArticleListCrawlerTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Crawler;
+
+class ArticleListCrawlerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testItExtractsArticlesFromFetchedContent()
+    {
+        $this->markTestSkipped('This test is waiting to be implemented.');
+
+        $articles = [new Article('Foo', 'foo.html')];
+
+        $contentProvider = $this->prophesize(ContentProvider::class);
+        $contentProvider->fetch('resource.html')->willReturn('<article/>');
+
+        $articleExtractor = $this->prophesize(ArticleExtractor::class);
+        $articleExtractor->extractArticles('<article/>')->willReturn($articles);
+
+        $articleListCrawler = new ArticleListCrawler($contentProvider->reveal(), $articleExtractor->reveal());
+        $returnedArticles = $articleListCrawler->findArticles('resource.html');
+
+        $this->assertSame($articles, $returnedArticles);
+    }
+
+    /**
+     * @expectedException \LogicException
+     * @expectedExceptionMessage Could not find articles in the resource: "resource.html"
+     */
+    public function testItThrowsAnExceptionIfNoArticleWasFound()
+    {
+        $this->markTestSkipped('This test is waiting to be implemented.');
+
+        $contentProvider = $this->prophesize(ContentProvider::class);
+        $contentProvider->fetch('resource.html')->willReturn('<article/>');
+
+        $articleExtractor = $this->prophesize(ArticleExtractor::class);
+        $articleExtractor->extractArticles('<article/>')->willThrow(new \LogicException());
+
+        $articleListCrawler = new ArticleListCrawler($contentProvider->reveal(), $articleExtractor->reveal());
+        $articleListCrawler->findArticles('resource.html');
+    }
+
+    /**
+     * @expectedException \LogicException
+     * @expectedExceptionMessage Could not fetch the resource: "resource.html"
+     */
+    public function testItThrowsAnExceptionIfResourceCouldNotBeFetched()
+    {
+        $this->markTestSkipped('This test is waiting to be implemented.');
+
+        $contentProvider = $this->prophesize(ContentProvider::class);
+        $contentProvider->fetch('resource.html')->willThrow(new \Exception());
+
+        $articleExtractor = $this->prophesize(ArticleExtractor::class);
+        $articleExtractor->extractArticles('<article/>')->willReturn([new Article('Foo', 'foo.html')]);
+
+        $articleListCrawler = new ArticleListCrawler($contentProvider->reveal(), $articleExtractor->reveal());
+        $articleListCrawler->findArticles('resource.html');
+    }
+}
