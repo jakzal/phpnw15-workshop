ede1bf268a26d69933e37238fa947182e2a65d35
Jakub Zalas <jakub@zalas.pl>
2015-10-01 19:37:00 +0100

    [Crawler][1.1] Implement the article class

--- tests/Crawler/ArticleTest.php
+++ tests/Crawler/ArticleTest.php
@@ -6,8 +6,6 @@ class ArticleTest extends \PHPUnit_Framework_TestCase
 {
     public function testItExposesArticleProperties()
     {
-        $this->markTestSkipped('This test is waiting to be implemented.');
-
         $article = new Article('Foo', '/foo.html');
 
         $this->assertSame('Foo', $article->getTitle());
